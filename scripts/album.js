bc7b469c383932dc6524e5ac2ca7474bb78cdd51
at-nichole <nichole.alexandria@gmail.com>
2017-12-24 10:30:02 -0600

    Completed assignment 25 practice

--- scripts/album.js
+++ scripts/album.js
@@ -45,7 +45,7 @@ var albumWarhol = {
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
   '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
@@ -70,11 +70,30 @@ var setCurrentAlbum = function(album) {
      }
  };
 
- window.onload = function() {
-     setCurrentAlbum(albumWarhol);
 
-     var albums = [albumWarhol, albumPicasso, albumMarconi];
-     var i = 0;
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+window.onload = function() {
+    setCurrentAlbum(albumMarconi);
+
+for (var i = 0; i < songRows.length; i++) {
+      songRows[i].addEventListener('mouseleave', function(event) {
+        this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+      });
+    }
+
+songListContainer.addEventListener('mouseover', function(event) {
+  if (event.target.parentElement.className === 'album-view-song-item') {
+        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+   });
+
+var albums = [albumWarhol, albumPicasso, albumMarconi];
+var i = 0;
+
      albumImage.addEventListener("click", function(change){
        setCurrentAlbum(albums[i]);
        i++;
@@ -83,3 +102,5 @@ var setCurrentAlbum = function(album) {
        }
      })
  };
+
+//
