722a7e623117c1d6de028e59f8ee81492bdc9d65
at-nichole <nichole.alexandria@gmail.com>
2017-12-27 15:43:54 -0600

    Checkpoint 28 Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,21 +1,62 @@
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+          opacity: 1,
+          transform: 'scaleX(1) translateY(0)'
+          });
+        };
 
-var points = document.getElementsByClassName('point');
-
-//var animatePoints = function(points) {
-
-var revealPoint = function(){
-forEach(points);
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function(){
-  if (window.innerHeight > 950) {
-    revealPoint();
-}
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-  window.addEventListener("scroll", function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            revealPoint();
+ $(window).load(function() {
+    if ($(window).height() > 950) {
+          animatePoints();
+    };
+  });
+
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+    $(window).scroll(function(event) {
+      if ($(window).scrollTop() >= scrollDistance) {
+         animatePoints();
         }
     });
+
+
+
+/* VANILLA JS
+
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+                var revealPoint = function( ) {
+                    for(var i = 0; i < 3; i++){
+                        points[i].style.opacity = 1;
+                        points[i].style.transform = "scaleX(1) translateY(0)";
+                        points[i].style.msTransform = "scaleX(1) translateY(0)";
+                        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+                    };
+                };
+
+                revealPoint();
 };
+
+window.onload = function() {
+
+    //Automatically animate the points on load when scrolling won't trigger animation
+    if(window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
+
+*/
